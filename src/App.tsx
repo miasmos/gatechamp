759e530f1b832458925a8bdd6526af6fa655aad1
Stephen Poole <stephenwpoole@gmail.com>
2023-02-24 13:12:13 -0500

    Remove static css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
-import "./App.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
