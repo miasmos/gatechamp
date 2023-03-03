ef40a030ef7a9f9f7cc9a3154cd13d0fd9fdad78
Stephen Poole <stephenwpoole@gmail.com>
2023-03-03 12:17:54 -0500

    Update hasStationSelector.ts

--- src/recoil/trip/selectors/hasStationSelector.ts
+++ src/recoil/trip/selectors/hasStationSelector.ts
@@ -1,11 +1,11 @@
 import { selector } from "recoil";
 import tripState from "../atom";
 
-const hasToStation = selector({
+const hasStationSelector = selector({
   key: "TripState:HasStation",
   get: ({ get }) =>
     get(tripState).to.some((value) => Boolean(value)) &&
     get(tripState).from.some((value) => Boolean(value)),
 });
 
-export default hasToStation;
+export default hasStationSelector;
